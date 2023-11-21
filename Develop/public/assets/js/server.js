5829b86fc25b02d8e1e1028a2a04e91f975bffd4
meerkat501 <leonardosoto911@gmail.com>
2023-11-21 14:53:39 -0600

    Added GET method to retrive notes and index HTML

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const {v4: uuidv4} = require('uuid');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+      });
+});
